ff78e1dbddfb7ee34769aff15e51e4fabbf11a55
npalomares <saucealito26@yahoo.com>
2015-09-12 15:02:27 -0700

    added .gitignore file along with some formatting mods to the index.php.

--- index.php
+++ index.php
@@ -19,18 +19,23 @@
 			$input_record_company, 
 			$input_cover_art_link,
 		);
-
 		//un-encrypted password
+
 
 		//encrypted password and repeat password
 
+
 		//validate
+
 
 		//username and password meet the minimum requirements
 
+
 		//run it
 
+
 		//if one result found, username is already taken
+
 
 		//check for valid email address
 
@@ -136,8 +141,8 @@
 	<section class="content container">
 		<div class="row">
 			<main role="main" class="main col-sm-8">
-				<h1 class="page-title">Page Title</h1>
-				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+				<h1 class="page-title">About the Author</h1>
+				<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt.</p>
 			</main>
 
 			<aside roles="complimentary" class="col-sm-4">
