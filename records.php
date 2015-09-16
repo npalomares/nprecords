acda3891a6111397d98c21a6c0709cc4d2641969
npalomares <saucealito26@yahoo.com>
2015-09-15 21:32:04 -0700

    WIP - adding pagination to the records page in order to organize better. Just need to refine the queries and map to the pagination set

--- records.php
+++ records.php
@@ -1,4 +1,122 @@
 <?php include('includes/header.php'); ?>
+<?php 
+	$tableName= "records";
+	$targetpage = "records.php";
+	$limit = 10;
+
+	$query = "SELECT COUNT(*) as num FROM $tableName";
+	$total_pages = mysql_fetch_array(mysql_query($query));
+	$total_pages = $total_pages['num'];
+
+	$stages = 3;
+	$page = mysql_escape_string($_GET['page']);
+	
+	if($page) {
+		$start = ($page - 1) * $limit;
+	}else{
+		$start = 0;
+	}
+
+	// Query the Data
+	$records_query = "SELECT * 
+					  FROM records
+					  ORDER BY artist ASC"; 
+
+	$records_result = mysql_query($records_query);
+
+	// Initial page num setup
+	if ($page == 0){$page = 1;}
+	$prev = $page - 1;
+	$next = $page + 1;
+	$lastpage = ceil($total_pages/$limit);
+	$LastPagem1 = $lastpage - 1;
+
+	$paginate = '';
+	if($lastpage > 1)
+	{
+
+	$paginate .= "<div class='paginate'>";
+
+	// Previous
+	if ($page > 1){
+	$paginate.= "<a href='$targetpage?page=$prev'>previous</a>";
+	}else{
+	$paginate.= "<span class='disabled'>previous</span>"; }
+
+	// Pages
+	if ($lastpage < 7 + ($stages * 2)) // Not enough pages to breaking it up
+	{
+	for ($counter = 1; $counter <= $lastpage; $counter++)
+	{
+	if ($counter == $page){
+	$paginate.= "<span class='current'>$counter</span>";
+	}else{
+	$paginate.= "<a href='$targetpage?page=$counter'>$counter</a>";}
+	}
+	}
+	elseif($lastpage > 5 + ($stages * 2)) // Enough pages to hide a few?
+	{
+	// Beginning only hide later pages
+	if($page < 1 + ($stages * 2))
+	{
+	for ($counter = 1; $counter < 4 + ($stages * 2); $counter++)
+	{
+	if ($counter == $page){
+	$paginate.= "<span class='current'>$counter</span>";
+	}else{
+	$paginate.= "<a href='$targetpage?page=$counter'>$counter</a>";}
+	}
+	$paginate.= "...";
+	$paginate.= "<a href='$targetpage?page=$LastPagem1'>$LastPagem1</a>";
+	$paginate.= "<a href='$targetpage?page=$lastpage'>$lastpage</a>";
+	}
+	// Middle hide some front and some back
+	elseif($lastpage - ($stages * 2) > $page && $page > ($stages * 2))
+	{
+	$paginate.= "<a href='$targetpage?page=1'>1</a>";
+	$paginate.= "<a href='$targetpage?page=2'>2</a>";
+	$paginate.= "...";
+	for ($counter = $page - $stages; $counter <= $page + $stages; $counter++)
+	{
+	if ($counter == $page){
+	$paginate.= "<span class='current'>$counter</span>";
+	}else{
+	$paginate.= "<a href='$targetpage?page=$counter'>$counter</a>";}
+	}
+	$paginate.= "...";
+	$paginate.= "<a href='$targetpage?page=$LastPagem1'>$LastPagem1</a>";
+	$paginate.= "<a href='$targetpage?page=$lastpage'>$lastpage</a>";
+	}
+	// End only hide early pages
+	else
+	{
+	$paginate.= "<a href='$targetpage?page=1'>1</a>";
+	$paginate.= "<a href='$targetpage?page=2'>2</a>";
+	$paginate.= "...";
+	for ($counter = $lastpage - (2 + ($stages * 2)); $counter <= $lastpage; $counter++)
+	{
+	if ($counter == $page){
+	$paginate.= "<span class='current'>$counter</span>";
+	}else{
+	$paginate.= "<a href='$targetpage?page=$counter'>$counter</a>";}
+	}
+	}
+	}
+
+	// Next
+	if ($page < $counter - 1){
+	$paginate.= "<a href='$targetpage?page=$next'>next</a>";
+	}else{
+	$paginate.= "<span class='disabled'>next</span>";
+	}
+
+	$paginate.= "</div>";
+
+	}
+	echo $total_pages.' Results';
+	// pagination
+	echo $paginate;			
+?>
 
 <section class="records-wrapper">
 	<div class="container">
@@ -8,8 +126,8 @@
 			<section class="articles col-sm-8">
 				<?php 
 					$records_query = "SELECT * 
-									  FROM records 
-									  ORDER BY record_id DESC";
+									  FROM records
+									  ORDER BY artist ASC"; 
 
 					$records_result = mysql_query($records_query);
 					while( $row_latest = mysql_fetch_array($records_result)) {
